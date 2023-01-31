d99eebbd641b7615e0258ecfd3613291ea4b20d8
iladem <124038433+iladem@users.noreply.github.com>
2023-01-31 13:01:07 +0100

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int anno;
+    printf("in che anno sei nato?\n");
+    scanf("%d",&anno);
+    
+    if(anno==1969)
+    {
+    printf("utente nato lo stesso anno in cui l'uomo è andato sulla Luna\n");
+    }
+    else if(anno<1969)
+    {
+    printf("utente nato prima dell'anno in cui l'uomo è andato sulla Luna di %d\n",1969-anno);
+    }
+    else
+    {
+    printf("utente nato dopo l'anno in cui l'uomo è andato sulla Luna di %d anni\n",anno-1969);
+    }
+    return 0;
+}
