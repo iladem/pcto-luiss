d8f83a5067dd1f18c7da5f4039785a65483add8b
iladem <124038433+iladem@users.noreply.github.com>
2023-02-01 11:59:58 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,28 +1,23 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
+void ft_ft(int*nbr)
+{
+    nbr=42;
+}
 int main()
 {
-    printf("inserisci due numeri\n");
-    int numero1;
-    int numero2;
-    scanf("%d",&numero1);
-    scanf("%d",&numero2);
-
-    if(numero1%numero2==0)
-{
-    printf("il primo numero è multiplo del secondo numero");
-}
-    else
-    {
-    printf("il primo numero non è multiplo del secondo numero");
-    }
+    int nbr=1;
+    
+    printf("il valore iniziale: %d\n",n);
+    ft_ft(&nbr);
+    printf("il valore effettivo dopo aver richiamato ft_ft: %d\n",n);
     return 0;
 }
