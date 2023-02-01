fbd29c5063217bcc12bf7f06c1e0812789c14eaa
iladem <124038433+iladem@users.noreply.github.com>
2023-02-01 12:37:25 +0100

    Add files via upload

--- main (3).c
+++ main (3).c
@@ -1,25 +1,30 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
+void ft_ft(int*nbr,int*x,int y)
+{
+    *nbr=42;
+    *x=10;
+    y=102;
+}
 int main()
 {
-    int anno;
-    printf("inserire un anno\n");
-    scanf("%d",&anno);
-    if((anno%4==0 && anno%100!=0)||anno%400==0)
-{
-    printf("l'anno è bisestile\n");
-}
-    else
-    {
-    printf("l'anno non è bisestile\n");
-    }
+    int nbr=2;
+    int x=7;
+    int y=3;
+    
+    printf("il valore iniziale: %d\n",x);
+    ft_ft(&x,&nbr, y);
+    
+    printf("il valore: %d\n",x);
+    printf("il valore: %d\n",y);
+    printf("il valore effettivo dopo aver richiamato ft_ft: %d\n",nbr);
     return 0;
 }
