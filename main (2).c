32bbaee6467e93fe7fb4c88dc9e3be2ce6aa8258
iladem <124038433+iladem@users.noreply.github.com>
2023-02-02 11:37:00 +0100

    Add files via upload

--- main (2).c
+++ main (2).c
@@ -1,27 +1,30 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
-void ft_ft(int*nbr,int*x)
-{
-    *nbr=42;
-    *x=10;
-}
 int main()
-{
-    int nbr=2;
-    int x=7;
-    
-    printf("il valore iniziale: %d\n",x);
-    ft_ft(&x, &nbr);
-    
-    printf("il valore: %d\n",x);
-    printf("il valore effettivo dopo aver richiamato ft_ft: %d\n",nbr);
+    {
+      int numero;
+      int potenza;
+      int risultato;
+      
+      printf("inserire un n da elevare alla potenza\n");
+      scanf("%d",&numero);
+      printf("di quanto vuoi elevare il numero\n");
+      scanf("%d",&potenza);
+      risultato=1;
+      
+      while(potenza>0)
+      {
+      risultato=risultato*numero;
+      potenza=potenza-1;
+      }    
+      printf("il risultato è:%d\n",risultato);
     return 0;
 }
