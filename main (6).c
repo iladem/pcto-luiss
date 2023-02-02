626ed4538908a4df6d47cb49f811cc8418ad52f8
iladem <124038433+iladem@users.noreply.github.com>
2023-02-02 16:47:01 +0100

    Add files via upload

--- main (6).c
+++ main (6).c
@@ -8,24 +8,27 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 #include <stdio.h>
 
+int divisione(int n1,int n2,int *quoziente,int *resto)
+{
+    *quoziente=n1/n2;
+    *resto=n1%n2;
+}
+
 int main()
 {
     int n1;
     int n2;
-    int n3;
-    printf("inserire n1\n");
-    scanf("%d",&n1);
-    printf("inserire n2\n");
-    scanf("%d",&n2);
-    printf("inserire n3\n");
-    scanf("%d",&n3);
-    
-    if(n1!=n2 && n2!=n3 && n3!=n1)
-    printf("il triangolo è scaleno\n");
-    else if(n1==n2 && n2==n3 && n3==n1)
-    printf("il triangolo è equilatero\n");
-    else 
-    printf("il triangolo è isoscele\n");
+    int quoziente;
+    int resto;
+    printf("inserisci n1 e n2\n");
+    scanf("%d %d",&n1,&n2);
+    if(n2==0)
+    {
+    printf("non puoi dividere per 0\n");
+    }
+    divisione(n1,n2,&quoziente,&resto);
+    printf("il quoziente è: %d\n",quoziente);
+    printf("il resto è: %d\n",resto);
     
     return 0;
 }
